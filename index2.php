495b4b176a43575456b103c9a611f9b8e989c803
changtianfei <changtianfei@itenscen.cn>
2018-04-11 14:20:44 +0800

    提交

---
+++ index2.php
@@ -0,0 +1,15 @@
+<script>
+	//判断是否是安卓手机
+	function checkPlatform(){
+		if(/android/i.test(navigator.userAgent)){
+		$("#image_file").attr('accept','image/*');
+		}
+	}
+	$(document).ready(function(){
+		checkPlatform();
+	});
+</script>
+
+
+
+Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36
